eb370ede4f8982e6336af3347e91753a09b24cf0
kalpesh <kalpesh@beepixl.com>
2021-07-27 14:17:21 +0530

    fixes

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -117,9 +117,9 @@ class CustomerController extends Controller
    if(!empty($request->business_name)){
        $user->business_name = $request->business_name;
    }
-   if(!empty($request->category_id)){
-       $user->category_id = $request->category_id;
-   }
+  //  if(!empty($request->category_id)){
+  //      $user->category_id = $request->category_id;
+  //  }
    if(!empty($request->name)){
         $user->name = $request->name;
    }
